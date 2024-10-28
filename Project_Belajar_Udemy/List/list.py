c205385f3586d7151ffde6dc5e35b696316df028
DavidsonRafaelK <davidson.rafael120@gmail.com>
2024-10-28 02:57:00 +0000

    Add Subject about list

---
+++ Project_Belajar_Udemy/List/list.py
@@ -0,0 +1,10 @@
+# Davidson Rafael Krisman Nugroho
+# 9:41 Mon, Oct 28 2024
+# List - List
+
+my_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # This is a list of number from 1 to 10
+print(my_number[0]) # This will print the first number in the list ( Remember a list always start from 0 ) Output : 1
+print(my_number[0:2]) # This will print the first number in the list to the second number in the list | Output : 1 to 2 ( n-1 )
+print(my_number[::]) # This will copy the list
+print(my_number[0::4]) # This will print a number in a list from the first number to the last number with a step of 4 | Output : 1, 5, 9
+print(my_number[::2]) # This will print a number in a list from the first number to the last number with a step of 2 | Output : 1, 3, 5, 7, 9
